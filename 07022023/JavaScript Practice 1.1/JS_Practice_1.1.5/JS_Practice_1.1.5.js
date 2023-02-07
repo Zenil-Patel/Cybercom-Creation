3520ffbdbde6c4adfb7a5171e95adad1a5a27197
Zenil-Patel <patelzenil1616@gmail.com>
2023-02-07 12:36:42 +0530

    JS practice_1.1.5

---
+++ 07022023/JavaScript Practice 1.1/JS_Practice_1.1.5/JS_Practice_1.1.5.js
@@ -0,0 +1,12 @@
+function changeImage1() {
+    var image = document.getElementById("v6Car");
+    image.src = "v6Engine.jpg";
+}
+function changeImage2() {
+    var image = document.getElementById("v8Car");
+    image.src = "v8Engine.jpg";
+}
+function changeImage3() {
+    var image = document.getElementById("v10Car");
+    image.src = "v10Engine.jpg";
+}
