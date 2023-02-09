3994bca7a9a67070978b4c05ca73c15beec97d6a
Zenil-Patel <patelzenil1616@gmail.com>
2023-02-09 11:42:52 +0530

    Completed JS Practice 1.4

--- 08022023/JS_Practice_1.4/JS_Practice_1.4_2.js
+++ 08022023/JS_Practice_1.4/JS_Practice_1.4_2.js
@@ -1,12 +1,17 @@
 // const ogStr = "<html><head></head></html>"
-const ogStr = "<html><head></head><body></boy></html>"
+// const ogStr = "<html><head></head><body></boy></html>"
+// const ogStr = "<html><br><head></head></html>"
+
+const ogStr = "<html><head><p>This is a para</p></head></html>"
+
 let str = ogStr;
 let arr = [];
+exceptionArr = ["br", "hr", "img", "input"];
 console.log(str);
 
+//Extracting the content of string betn "<"" and ">"
 let indexOf_lesser = 0;
 let indexOf_greater = 0;
-
 // debugger;
 for (i = 0; i < str.length; i++) {
     if (str[i] == "<") {
@@ -24,28 +29,54 @@ for (i = 0; i < str.length; i++) {
 }
 console.log(arr);
 
-let arr1 = [];
-
+let arr_dup = [];
 for (i = 0; i < arr.length; i++) {
     if (arr[i]) {
-        arr1.push(arr[i]);
+        arr_dup.push(arr[i]);
     }
 }
-console.log(arr1);
+console.log(arr_dup);
 
-arr2 = [];
+//Remove all the duplicates of arr_dup by converting into set 
+let set = new Set(arr_dup);
+//Convert srt to array
+arr1 = Array.from(set);
+
+console.log(arr1);
+// debugger;
+
+//Will remove the elements which are in exceptionArr
+arr1_excep = [];
 for (i = 0; i < arr1.length; i++) {
-    if (arr1[i][0] == "/") {
-        if (arr1[i].slice(1, ) == arr2.slice(-1)) { arr2.pop() }
+    let addEle = 1;
+    for (j = 0; j < exceptionArr.length; j++) {
+        if (arr1[i] == exceptionArr[j]) {
+            addEle = 0;
+            break;
+        }
+    }
+    if (addEle != 0) {
+        arr1_excep.push(arr1[i]);
+    }
+}
+
+console.log(arr1_excep);
+
+//To check whether the opening element has its corresponding closing tag
+arr2 = [];
+for (i = 0; i < arr1_excep.length; i++) {
+    if (arr1_excep[i][0] == "/") {
+        if (arr1_excep[i].slice(1,) == arr2.slice(-1)) { arr2.pop() }
         else { console.log("Invalid"); break; }
     }
     else {
-        arr2.push(arr1[i]);
+        arr2.push(arr1_excep[i]);
     }
 }
 console.log(arr2);
 if (arr2.length == 0) {
     console.log("Valid");
 }
+
 
 
