66631f9b5a30b4e85b9ea1ea0b15fe8012241e3b
Zenil-Patel <patelzenil1616@gmail.com>
2023-02-10 15:55:09 +0530

    Completed JS Practice 1.6

--- 10022023/js_practice_1.6/js_practice_1.6_1/js_practice_1.6_1.js
+++ 10022023/js_practice_1.6/js_practice_1.6_1/js_practice_1.6_1.js
@@ -1,21 +1,5 @@
 let nums = [1, 2, 3];
 let powerSet = [[]];
-console.log(powerSet.length);
-// for (let i=0; i<nums.length; i++){
-//     for( let j=0, l=powerSet.length; j<l; j++){
-//         powerSet.push(powerSet[j].concat(nums[i]));
-//     }
-// }
-
-//little changes
-// for (let i=0; i<nums.length; i++){
-//     for( let j=0; j<powerSet.length; j++){
-//         powerSet.push(powerSet[j].concat(nums[i]));
-//     }
-// }
-// console.log(powerSet);
-
-
 
 for (let i=0; i<nums.length; i++){
     for( let j=0, l=powerSet.length; j<l; j++){
--- 10022023/js_practice_1.6/js_practice_1.6_2/js_preactice_1.6_2.js
+++ 10022023/js_practice_1.6/js_practice_1.6_2/js_preactice_1.6_2.js
@@ -1,9 +1,28 @@
-let db = [{'A': 1}]
-let code = '11101';
-uniqueCode = code.split("");
-console.log(uniqueCode);
-for(i=0; i<uniqueCode.length-1;i++){
-    for(j=i+1; j<uniqueCode.length;j++){
-        console.log()
+console.log("Code is valid only for 3 digits.")
+let code = '123';
+let uniqueCode = code.split("");
+let tempArr = [];
+let secondaryArr = [];
+if(code==0){ 
+    console.log("Found value = 0"); 
+}else{
+
+//Copied uniqueCode array in tempArr
+tempArr = [].concat(uniqueCode);
+
+
+for(let i=0; i<tempArr.length-1; i++){
+    let temp = [].concat(tempArr)
+
+    temp.splice(i,2, tempArr[i]+tempArr[i+1]);
+    let re0 = parseInt(tempArr[i]);
+    let re26 = parseInt(tempArr[i]+tempArr[i+1]);
+    //Add the values which are below 26 and above 0
+    if(re0>0 && re26 <= 26){
+        secondaryArr.push(temp);
     }
+}
+secondaryArr.push(uniqueCode);
+console.log(secondaryArr);
+
 }
