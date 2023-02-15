349daf34ab0980dbcea1e19eb04bdc6b885c0f71
Zenil-Patel <patelzenil1616@gmail.com>
2023-02-15 15:56:31 +0530

    Modified files in Evaluation test

--- 14022023/Evaluation Test/JS/functionalites.js
+++ 14022023/Evaluation Test/JS/functionalites.js
@@ -20,14 +20,45 @@ function logout() {
 
 
 retriveData = JSON.parse(localStorage.getItem('Users'));
-
+let bdateArr = [];
 for (let i = 0; i < retriveData.length; i++) {
     let date = new Date();
 
     strDate = JSON.stringify(date);
-    currentDate = strDate.slice(6, 11)
+    var currentDate = strDate.slice(6, 11)
     let bDate = (retriveData[i].Birthdate).slice(5,11);
+    bdateArr.push(bDate);
     if (currentDate == bDate){
         document.getElementById('displayBday').innerHTML = "Today is '"+retriveData[i].Name+"' Birthday."
     }
 }
+
+//Array of ages of users
+let ageArr = [];
+for (let i=0; i<bdateArr.length; i++){
+    ageArr.push(parseInt(currentDate)-parseInt(bdateArr[i]));
+}
+let below18Arr = [];
+let abover50Arr = [];
+let betnArr = [];
+
+let countBelow18 = 0;
+let countabove50 = 0;
+let countbetn = 0;
+for (let i=0; i<ageArr.length; i++){
+    if(ageArr[i]<18){
+        below18Arr.push(ageArr[i]);
+        countBelow18++;
+    }
+    if(ageArr[i]>18 && ageArr[i]<50){
+        betnArr.push(ageArr[i]);
+        countbetn++;
+    }
+    if(ageArr[i]>50){
+        abover50Arr.push(ageArr[i]);
+        countabove50++;
+    }
+}
+document.getElementById('agePara1').innerHTML= countBelow18;
+document.getElementById('agePara2').innerHTML= countbetn;
+document.getElementById('agePara3').innerHTML= countabove50;
